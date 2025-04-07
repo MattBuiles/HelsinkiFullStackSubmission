3553a2cc19cacd2fe885121121ef82ba4e2a4129
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2025-04-06 22:15:15 -0500

    Incorporación de la parte 3

---
+++ part3/phonebook_backend/README.md
@@ -0,0 +1,6 @@
+# My Web Application
+
+Welcome to my web phonebook application! This repository contains the source code for a web-based application.
+
+## Live Demo
+You can access the live application here: [My Web App](https://helsinkifullstack-bwzx.onrender.com/)
---
+++ part3/phonebook_backend/models/person.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+
+if (!process.env.MONGODB_URI) {
+  console.error('MONGODB_URI environment variable is required')
+  process.exit(1)
+}
+else {
+  console.log('MONGODB_URI environment variable is set')
+}
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! Format: XX-XXXXXXX or XXX-XXXXXXX`
+    }
+  }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
